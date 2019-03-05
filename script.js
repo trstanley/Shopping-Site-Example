d7d191b76fb78bdfc48c05897f1b67cda7d985bb
trstanley <31667190+trstanley@users.noreply.github.com>
2019-03-05 09:05:27 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+
+$(".stroller-button").click(function(){
+    $(".img").show();
+    $(".img").attr("src","https://www.nicepng.com/png/full/16-165624_uppababy-uppababy-stroller-and-bassinet.png");
+    $(".price").show();
+    $(".price").text("Price: $350");
+    $("#buy-stroller").show();
+});
+
+$(".carseat-button").click(function(){
+    $(".img").show();
+    $(".img").attr("src","https://www.nuna.eu/media/catalog/product/cache/1/image/1800x/040ec09b1e35df139433887a97daa66f/p/i/pipa_night_angle_right_us_-_tisrev_8-31.png");
+    $(".price").show();
+    $(".price").text("Price: $200");
+});
+
+$(".crib-button").click(function(){
+    $(".img").show();
+    $(".img").attr("src","https://johnlewis.scene7.com/is/image/JohnLewis/237736321?$rsp-plp-port-320$");
+    $(".price").show();
+    $(".price").text("Price: $400");
+});
+
+$(".chair-button").click(function(){
+    $(".img").show();
+    $(".img").attr("src","http://www.pngmart.com/files/7/High-Chair-PNG-Transparent-Image.png");
+    $(".price").show();
+    $(".price").text("Price: $100");
+});
+
+$(".sophie-button").click(function(){
+    $(".img").show();
+    $(".img").attr("src","http://tinyworld.com.au/image/cache/data/tinyworld/products/vulli/web/616324-itself-500x500.png");
+    $(".price").show();
+    $(".price").text("Price: $10");
+});
+
+$("#buy-stroller").click(function(){
+    $(".cart-item1").text("Stroller: $350");
+});
+
+
